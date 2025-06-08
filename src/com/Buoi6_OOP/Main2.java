8877937e2d913469af066da96963ab375f09f82b
backend <huynhhieu210106@gmail.com>
2025-06-08 16:23:18 +0700

    Buoi6_Commit3: Demo ArrayList

---
+++ src/com/Buoi6_OOP/Main2.java
@@ -0,0 +1,37 @@
+package com.Buoi6_OOP;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<SinhVien> listStudent = new ArrayList<>();
+        do {
+            System.out.println("------ ");
+            SinhVien sv = new SinhVien();
+            System.out.print("Tên Sinh Viên = ");
+            sc.nextLine();
+            sv.fullName = sc.nextLine();
+            System.out.print("Giới Tính = ");
+            sv.gender = sc.nextBoolean();
+            sc.nextLine();
+            System.out.print("Chuyên Ngành = ");
+            sv.major = sc.nextLine();
+            System.out.println("------ ");
+            System.out.print("Nhập tiếp hông (y/n) >> ");
+            String answer = sc.next();
+            listStudent.add(sv);
+            if (!answer.equals("y")) {
+                break;
+            }
+        } while (true);
+
+        listStudent.forEach(e ->{
+            System.out.println("Tên Sinh Viên = "+ e.fullName);
+            System.out.println("Giới Tính = "+ e.gender);
+            System.out.println("Chuyên Ngành = "+ e.major);
+            System.out.print("--------------------------- ");
+        });
+    }
+}
