ec1083cd95665d4efef77066943def997d5137ba
backend <huynhhieu210106@gmail.com>
2025-06-08 15:23:44 +0700

    Buoi6_Commit1:Update new information for repository

---
+++ src/com/Buoi3_JavaCore/haiSoNguyen.java
@@ -0,0 +1,29 @@
+package com.Buoi3_JavaCore;
+
+import java.util.Scanner;
+
+public class haiSoNguyen {
+    public static void main (String args []){
+        Scanner scanner = new Scanner (System.in) ;
+
+        System.out.println ("Số a : ");
+        double soA = scanner.nextDouble ();
+        System.out.println ("Số b : ");
+        double soB = scanner.nextDouble ();
+
+        double tong = soA + soB ;
+        double hieu = soA - soB ;
+        double tich = soA * soB ;
+        double thuong = soA / soB ;
+
+        System.out.println ("Tổng : " + tong );
+        System.out.println ("Hiệu : " + hieu );
+        System.out.println ("Tích : " + tich );
+        if (soB != 0){
+            System.out.println ("Thương : " + thuong );
+        } else {
+            System.out.println ("Không chia hết cho 0.");
+        }
+    }
+}
+// kì vậy ta
